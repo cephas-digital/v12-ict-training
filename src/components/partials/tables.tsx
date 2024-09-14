58f693c2a20a59a37ece3f78cf8d3b41c6790062
kaytwolea <paulkolapo8@gmail.com>
2024-09-14 17:13:45 +0100

    Update tables.tsx

--- src/components/partials/tables.tsx
+++ src/components/partials/tables.tsx
@@ -7,58 +7,61 @@ import Bad from "../../assets/icons/bad.svg";
 const ProductTable = ({ products, start }) => {
   return (
     <div className="">
-      <table className="min-w-full table-auto bg-white">
-        <thead>
-          <tr className="bg-[#F8FAFC] text-left">
-            <th className="py-4 text-sm font-semibold text-gray-800">
-              Product Info
-              <button className="ml-1">
-                <img src={Arrows} alt="" className="" />
-              </button>
-            </th>
-            <th className="py-4 text-sm text-right font-semibold text-gray-800">
-              Status
-              <button className="ml-2">
-                <img src={Arrows} alt="" className="" />
-              </button>
-            </th>
-          </tr>
-        </thead>
+      <div
+        style={{
+          borderBottom: "1px solid #F1F5F9",
+        }}
+        className="flex px-1 justify-between items-center h-8 w-full bg-[#F8FAFC]"
+      >
+        <div className="flex gap-1 items-center">
+          <h6 className="text-base font-medium text-da-blue-600">
+            Product Info
+          </h6>
+          <img src={Arrows} alt="" className="" />
+        </div>
+        <div className="flex gap-1 items-center">
+          <h6 className="text-base font-medium text-da-blue-600">Status</h6>
+          <img src={Arrows} alt="" className="" />
+        </div>
+      </div>
 
-        <tbody>
-          {products.map((product, index) => (
-            <tr
-              key={index}
-              className={`${index % 2 === 0 ? "bg-white" : "bg-[#F8FAFC]"}`}
-            >
-              <td className="py-4 text-da-blue-600 text-xs whitespace-nowrap">
-                {product.name}
-              </td>
-              <td className="py-4 flex justify-end">
-                {!start && (
-                  <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#D2D7D4]">
-                    <img src={Pending} alt="" className="" />
-                  </button>
-                )}
-
-                {start && (
-                  <>
-                    {product?.status === "good" ? (
-                      <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#BAFED2]">
-                        <img src={Good} alt="" className="" />
-                      </button>
-                    ) : (
-                      <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#EF444433]">
-                        <img src={Bad} alt="" className="" />
-                      </button>
-                    )}
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {products.map((product, index) => (
+          <div
+            style={{
+              borderBottom: "1px solid #F1F5F9",
+            }}
+            key={index}
+            className={`${
+              index % 2 === 0 ? "bg-white" : "bg-[#F8FAFC]"
+            } flex justify-between px-1 h-8 items-center w-full`}
+          >
+            <span className=" text-da-blue-600 text-xs truncate w-5/6">
+              {product.name}
+            </span>
+            <div className="flex justify-end">
+              {!start && (
+                <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#D2D7D4]">
+                  <img src={Pending} alt="" className="" />
+                </button>
+              )}
+              {start && (
+                <>
+                  {product?.status === "good" ? (
+                    <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#BAFED2]">
+                      <img src={Good} alt="" className="" />
+                    </button>
+                  ) : (
+                    <button className="w-[25px] h-[25px] flex justify-center items-center text-xl font-bold bg-[#EF444433]">
+                      <img src={Bad} alt="" className="" />
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
